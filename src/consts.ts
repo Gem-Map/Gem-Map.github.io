aab868c5fdce6e275955d7efff200d94345fddc6
kwakjoohyeong <rhkrwngud445@gmail.com>
2026-04-24 22:21:24 +0900

    init project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Gem있는 Writing';
+export const SITE_DESCRIPTION = '보석처럼 빛나는 사이드 프로젝트 팀 블로그';
