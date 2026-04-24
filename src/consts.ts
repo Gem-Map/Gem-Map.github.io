227c9965b6303315e0a785d2d8f0849524dcecd6
kwakjoohyeong <rhkrwngud445@gmail.com>
2026-04-24 22:38:37 +0900

    feat: design system improvement

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gem있는 Writing';
-export const SITE_DESCRIPTION = '보석처럼 빛나는 사이드 프로젝트 팀 블로그';
+export const SITE_TITLE = 'Gemmap';
+export const SITE_DESCRIPTION = '숨겨진 사진 스팟을 찾는 앱, Gemmap의 팀 블로그입니다. 보석 같은 기록들을 담는 "Gem있는 Writing" 공간입니다.';
