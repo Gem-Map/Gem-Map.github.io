562838a480f5e7f9b1318a98e998a52b3582f658
kwakjoohyeong <rhkrwngud445@gmail.com>
2026-07-01 00:14:43 +0900

    feat: 문의 및 지원 이메일 정보 추가 (hiddenplaceseeker@gmail.com)

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'Gemmap';
 export const SITE_DESCRIPTION = '숨겨진 사진 스팟을 찾는 앱, Gemmap의 팀 블로그입니다. 보석 같은 기록들을 담는 "Gem있는 Writing" 공간입니다.';
+export const SUPPORT_EMAIL = 'hiddenplaceseeker@gmail.com';
+
