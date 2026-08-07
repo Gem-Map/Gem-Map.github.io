c9ad3dd85d01c176cc5972c195806ce8251698a8
kwakjoohyeong <rhkrwngud445@gmail.com>
2026-08-07 23:47:38 +0900

    feat: add hidden /download redirect page for iOS/Android app stores

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,8 @@ export const SITE_TITLE = 'Gemmap';
 export const SITE_DESCRIPTION = '숨겨진 사진 스팟을 찾는 앱, Gemmap의 팀 블로그입니다. 보석 같은 기록들을 담는 "Gem있는 Writing" 공간입니다.';
 export const SUPPORT_EMAIL = 'hiddenplaceseeker@gmail.com';
 
+// App Store & Play Store URLs for OS redirection (/download page)
+export const APP_STORE_URL = 'https://apps.apple.com/app/gemmap'; // TODO: 실제 iOS App Store URL로 필요시 수정
+export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.gemmap.app'; // TODO: 실제 Android Play Store URL로 필요시 수정
+
+
