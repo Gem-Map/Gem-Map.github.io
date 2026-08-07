f8dbf365a6b1db0726a6267cfc2fd010ef73a4eb
kwakjoohyeong <rhkrwngud445@gmail.com>
2026-08-07 23:51:03 +0900

    fix: update official App Store and Google Play Store URLs

--- src/consts.ts
+++ src/consts.ts
@@ -6,7 +6,6 @@ export const SITE_DESCRIPTION = '숨겨진 사진 스팟을 찾는 앱, Gemmap�
 export const SUPPORT_EMAIL = 'hiddenplaceseeker@gmail.com';
 
 // App Store & Play Store URLs for OS redirection (/download page)
-export const APP_STORE_URL = 'https://apps.apple.com/app/gemmap'; // TODO: 실제 iOS App Store URL로 필요시 수정
-export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.gemmap.app'; // TODO: 실제 Android Play Store URL로 필요시 수정
-
+export const APP_STORE_URL = 'https://apps.apple.com/kr/app/gemmap-%EC%88%A8%EC%9D%80-%ED%8F%AC%ED%86%A0-%EC%8A%A4%ED%8C%9F-%EB%B0%9C%EA%B2%AC%ED%95%98%EA%B3%A0-%EC%B0%BE%EC%95%84%EA%B0%80%EA%B8%B0/id6754697654';
+export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.cheongha.gemmap.gem_map';
 
